28dffc1269338eb68d28cbe8618450db39a2258c
Devin Bunker <devin.bunker@gmail.com>
2020-09-11 18:57:19 -0600

    add parameters to pancake funcs, add comments

--- pancake/pancake.go
+++ pancake/pancake.go
@@ -3,14 +3,32 @@ package pancake
 type Stack struct {
 }
 
-func (s Stack) Flip() {
+// Peek examines an individual pancake in the list,
+// returning whether it is face up. If the i
+// parameter is beyond the current extents of the
+// stack, then false will be returned in the second
+// return value.
+func (s Stack) Peek(i int) (bool, bool) {
 
 }
 
-func (s Stack) Add() {
+// Flip flips over the stack from the top to the
+// pancake at position i.
+// If the i parameter is beyond the extents of the
+// stack, then false is returned.
+func (s Stack) Flip(i int) (bool) {
 
 }
 
+// Add appends new pancakes to the stack, with
+// each boolean argument specifying whether the
+// pancake is face up.
+func (s Stack) Add(faceUp ...bool) {
+
+}
+
+// Normalize performs the necessary operations to
+// make sure every pancake in the stack is face up.
 func (s Stack) Normalize() {
 
 }
